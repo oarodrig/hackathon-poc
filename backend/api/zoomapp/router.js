0ebf5729f8be9cafc0a7614beaf1e379036e16cb
oarodrig <oarodriguez.cs@gmail.com>
2022-12-06 10:37:01 -0800

    Add files via upload

---
+++ backend/api/zoomapp/router.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+const router = Router()
+const controller = require('./controller')
+router
+  .use('/proxy', controller.proxy)
+  .use('/sockjs-node', controller.proxy)
+  .get('/install', controller.install)
+  .get('/auth', controller.auth)
+  .get('/home', controller.home)
+  .get('/attention', controller.attention)
+  .get('/authorize', controller.inClientAuthorize)
+  .post('/onauthorized', controller.inClientOnAuthorized)
+
+module.exports = router
